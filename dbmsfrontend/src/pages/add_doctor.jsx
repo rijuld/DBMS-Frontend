ec43083fcdfc9984f7b5b923a600dc178356a97e
Saloni <s.saloni1401@gmail.com>
2021-04-15 01:42:01 +0530

    Trying to add patient phone number

--- dbmsfrontend/src/pages/add_doctor.jsx
+++ dbmsfrontend/src/pages/add_doctor.jsx
@@ -7,3 +7,17 @@ import "react-toastify/dist/ReactToastify.css";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import qs from "qs";
+
+function Add_doctor(){
+    const [did, setdid] = useState("");
+  const [post, setpost] = useState("0");
+  const [dob, setdob] = useState("0");
+  const [first_name, setfname] = useState("0");
+  const [last_name, setlname] = useState("");
+  
+  const [gender, setgender] = useState("");
+  const [dept_id, setdept_id] = useState("");
+  const [password, setpassword] = useState("");
+  const [street, setstreet] = useState("");
+  const [house, sethouse] = useState("");
+}
