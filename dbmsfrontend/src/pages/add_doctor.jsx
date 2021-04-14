b6e05293defcace307f6d8d57e72365515a45e17
Saloni <s.saloni1401@gmail.com>
2021-04-15 00:37:12 +0530

    Removed errors on run

---
+++ dbmsfrontend/src/pages/add_doctor.jsx
@@ -0,0 +1,9 @@
+
+import React from "react";
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import qs from "qs";
